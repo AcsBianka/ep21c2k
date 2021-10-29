b314d3e30ef82806c9ed1a6d06648f94dc41823b
AcsBianka <aacsbianka@gmail.com>
2021-10-29 16:37:42 +0200

    1. zh

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ for line in fileobject:
 print("A sorok száma: ", count_rows-1)
 
 #3. feladat:
-jo_mego = fileobject.readline()
+
 versenyzo_valasz = []
+jo_mego = fileobject.readline()
+
 input_versenyzo = input("Kérem adja meg a versenyző azonítóját: ")
 valasz = ""
 for i in range(count_rows):
-    if(versenyzo_valasz[i][0:5] == input_versenyzo):
+    if(input_versenyzo == versenyzo_valasz[i][0:5]):
         valasz=versenyzo_valasz[i][6:20]
         print(valasz)
     else:
@@ -23,10 +25,9 @@ for i in range(count_rows):
 
 #4. feladat:
 print("A helyes megoldás: ")
-print(jo_mego)
 jo_valasz = ""
 for i in range(14):
-    if(valasz[i] == jo_mego[i]):
+    if(jo_mego[i] == valasz[i]):
         jo_valasz = jo_valasz + '+'
     else:
         jo_valasz = jo_valasz + ' '
@@ -37,20 +38,13 @@ sorszam_input = input("Kérem adja meg a feladat sorszámát: ")
 try:
     jo_db = 0
     sorszam = int(sorszam_input)
-    sorszam_i = sorszam - 1
+    sorszam_i = sorszam-1
     for i in range(count_rows):
             if(jo_mego[sorszam_i] == versenyzo_valasz[i][sorszam_i+6]):
                 jo_db += 1
-    jo_szazalek = (jo_db / count_rows) * 100
+    jo_szazalek = (jo_db/count_rows)*100
     print("A válaszra fő, a versenyzők %-a adott helyes választ.", jo_db, jo_szazalek)
 
 except ValueError:
-    print("Hibás sorszám")
-
-
-
-print("A versenyzők pontszámának a meghatározása.")
-pontszamok = []
-#for i in range(count_rows):
- #   pontszamok.append(0)
+    print("Rossz sorszámot adott meg.")
 
